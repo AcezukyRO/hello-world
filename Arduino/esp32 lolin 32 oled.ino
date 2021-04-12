7de618a5406b9e3c499b0322b4848712d63a8af3
rock-on <36922874+AcezukyRO@users.noreply.github.com>
2021-04-12 23:41:31 +0700

    Add files via upload

---
+++ Arduino/esp32 lolin 32 oled.ino
@@ -0,0 +1,42 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSans9pt7b.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+ 
+void setup() {
+  Serial.begin(115200);
+  
+  // Start I2C Communication SDA = 5 and SCL = 4 on Wemos Lolin32 ESP32 with built-in SSD1306 OLED
+  Wire.begin(5, 4);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false, false)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000); // Pause for 2 seconds
+ 
+  // Clear the buffer.
+  display.clearDisplay();
+
+  display.setFont();
+  display.setTextSize(1.5);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 30);
+  // Display static text
+  display.println("Thanks Lardon! :3");
+  display.display(); 
+}
+ 
+void loop() {
+  
+}
